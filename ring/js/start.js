32101907d2f1110cb037dc03aaa23ca8b23be871
ankithaoff <75296914+ankithaoff@users.noreply.github.com>
2020-12-03 22:52:29 +0530

    my first commit

---
+++ ring/js/start.js
@@ -0,0 +1,14 @@
+
+        new WOW().init();
+
+
+
+
+        function hover(element) {
+                element.setAttribute('src', 'img/main2.jpg');
+              }
+
+        
+              function unhover(element) {
+                element.setAttribute('src', 'img/aboutus.jpg');
+              }
